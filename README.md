cd918ea7ef6ac282677d950019723793885ff9c7
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-06-08 19:26:51 -0400

    wrote out the readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# password-generator
+# Password Generator
+
+## The Goal
+
+Create a password generator, using javascript, that imitates provided example. 
+Generator should create a password of the desired length between 8 and 128 characters, using only the selected character types.
+
+## What I've Done
+
+Imitated example to the best of my ability, by building on the existing code. Added a prompt that asks for character length and types, 
+a function that builds the character array using by the generator based on user input, and generates the password itself.
+Password is then written to the site's html. Commented out the script to make it easy to follow.
+
+Bonus: Generator wipes previously generated password and character array when generating a new password. So you can use it to generate multiple!
+
+## Usage
+
+Hit the generate button and follow the prompts. Now you have a secure password for use wherever!
+
+## Screenshots
+
--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 //Array for each character type
-var lowerCase = ("abcdefghijklmnopqrstuvwxyz").split("");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 
-var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split("");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-var integer = ("1234567890").split("");
+var integer = "1234567890".split("");
 
-var special = ("~!@#$%^&*()_+").split("");
-
+var special = "~!@#$%^&*()_+".split("");
 
 //Array to hold selected character types
-var masterArray = []
+var masterArray = [];
 
 //string for holding generated password
-let newPass = ""
+let newPass = "";
 
 //string for password length input
-let passwordLength = ""
+let passwordLength = "";
 
-//the generator function! 
+//the generator function!
 function generatePassword() {
   //prompts the user to input password length
-  var passwordLength = prompt("How many characters in password? Minimum 8, Maximum 128.")
+  var passwordLength = prompt(
+    "How many characters in password? Minimum 8, Maximum 128."
+  );
   //ends function if input is incorrect
-    if (isNaN(passwordLength) || passwordLength <8 || passwordLength > 128 ) {
-      alert("please select a number between 8 and 128.");
-      return;
-    }
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("please select a number between 8 and 128.");
+    return;
+  }
   
   //confirm selections
-  var confirmLow = confirm('Include lowercase letters?');
-  var confirmUpp = confirm('Include uppercase letters?');
-  var confirmNum = confirm('Include numbers?');
-  var confirmSpe = confirm('Include special characters?');
+  var confirmLow = confirm("Include lowercase letters?");
+  var confirmUpp = confirm("Include uppercase letters?");
+  var confirmNum = confirm("Include numbers?");
+  var confirmSpe = confirm("Include special characters?");
   
   //concatenate selections to master Array
   if (confirmLow) {
@@ -50,9 +50,7 @@ function generatePassword() {
   if (confirmSpe) {
     masterArray = masterArray.concat(special);
   }
-
-  console.log(masterArray);
-
+  
   //ends function if no characters were selected
   if (!confirmLow && !confirmUpp && !confirmSpe && !confirmNum) {
     alert("You must select at least one character type.");
@@ -67,9 +65,14 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
+  //clears any info that may exist in our array/string due to previously generated passwords
   newPass = "";
   masterArray = [];
+  
+  //generates password
   generatePassword();
+  
+  //writes to text box
   var passwordText = document.querySelector("#password");
   passwordText.value = newPass;
 }
